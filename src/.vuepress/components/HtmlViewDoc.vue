3ad7e5c64554c7921b46f1d0929ae70d034889ec
Gizem Ulubayam <gizemulubayam@gmail.com>
2018-11-14 09:37:53 +0300

    added HtmlView component doc

---
+++ src/.vuepress/components/HtmlViewDoc.vue
@@ -0,0 +1,15 @@
+<template>
+  <HtmlView html="<div><h1>HtmlView</h1></div>" />
+</template>
+
+<script>
+import { HtmlView } from "nativescript-vue-web";
+export default {
+  name: "HtmlViewDoc",
+  components: { HtmlView }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
